e63581a66e4217cc57e073534b8245ca871ef330
Snunes179 <samuel@ipextreme.com.br>
2021-10-27 17:43:11 -0300

    Flappy Bird Game Base Without Keyboard Commands 27-10-2021

--- game.py
+++ game.py
@@ -6,8 +6,8 @@ import random
 
 
 # define global variables
-WINDOW_WIDTH = 570 # window width size
-WINDOW_HEIGHT = 700 # window height size
+WINDOW_WIDTH = 500 # window width size
+WINDOW_HEIGHT = 800 # window height size
 
 
 BIRD_IMGS = [ # load every bird image in the 'imgs' folder and scale 2x
@@ -19,7 +19,7 @@ PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png'))) # load the base img and scale 2x
 BACKGROUND_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'background.png'))) # load the background img and scale 2x
 
-# bird class
+# bird object class
 class Bird:
     IMGS = BIRD_IMGS # load bird imgs inside the class
     MAX_ROTATION = 25 # controls the rotation of the bird as like the nose up when jumps, and etc..
@@ -95,28 +95,137 @@ class Bird:
     
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
+    
+# pipe object class
+class Pipe:
+    GAP = 200 # this is the spacing between pipes, just the place where the bird must pass
+    VELOCITY = 5 # this is how fiast the pipes will move in the screen
+    
+    def __init__(self, x): # initializes the pipe
+        self.x = x # define the pipe position on the screen
+        self.height = 0 # define it's height
+        self.gap = 100 # define the gap where the bird must pass
+        
+        self.top = 0 # define position that the flipped pipe must be
+        self.bottom = 0 # define position that the pipe must be
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True) # flip the pipe, to make the upside down pipe
+        self.PIPE_BOTTOM = PIPE_IMG # get the img for the normal pipe placed ate the bottom
+        
+        self.passed = False # check if the bird passes the pipe without collide
+        self.set_height() # set the pipe height and the gap
+        
+    def set_height(self): # set the height of the pipes and calculates the GAP of it
+        self.height =  random.randrange(50,450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+        
+    def move(self): # move the pipe foward to the bird
+        self.x -= self.VELOCITY
+        
+    def draw(self, win): # draw the pipes on the screen
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+    
+    def collide(self, bird): # check the collide between the bird and the pipe
+        bird_mask = bird.get_mask() # get the bird mask from it's image
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP) # get the mask from the upside down pipe
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM) # get the mask from the normal pipe
+        
+        top_offset = (self.x - bird.x, self.top - round(bird.y)) # get the distance between the bird and the top pipe
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y)) # get the distance between the bird and the bottom pipe
+        
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset) # check the pixels for collision between bird and the bottom pipe
+        t_point = bird_mask.overlap(top_mask, top_offset) # check the pixels for collision between bird and the top pipe
+        
+        if t_point or b_point: # if the collisions is not none, we set the collision as True
+            return True
 
+        return False # if not collision is False
+    
+# base object class
+class Base:
+    VELOCITY = 5 # set the velocity that the base will move
+    WIDTH = BASE_IMG.get_width() # set the width of the base img
+    IMG = BASE_IMG # get the base img
+    
+    def __init__(self, y): # initializes the base image
+        self.y = y # define the position of the base
+        self.x1 = 0 # define the width of the first base image
+        self.x2 = self.WIDTH # define the width of the second base image
+        
+    def move(self): # function that will move the base on the screen
+        self.x1 -= self.VELOCITY # modify the velocity of the base for the first image
+        self.x2 -= self.VELOCITY # modify the velocity of the base for the second image
+        
+        if self.x1 + self.WIDTH < 0: # if the first image go outside of the screen, it will go to after the  second image
+                                     # that will create the efect of infinite base image, or at least a very very very large image
+            self.x1 = self.x2 + self.WIDTH
+            
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+    
+    def draw(self, win): # function that will draw our images inside the screen
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+        
 # draw the background image and the bird
-def draw_window(win, bird):
+def draw_window(win, bird, pipes, base):
     win.blit(BACKGROUND_IMG, (0,0)) # draw the background with the topleft corner as the initial point
+    
+    for pipe in pipes: # for each pipe inside our object
+        pipe.draw(win) # this will draw the pipes
+        
+    base.draw(win) # draw the base on the screen
     bird.draw(win) # call the draw method on bird object
     pygame.display.update() # update the screen
 
 # main function is the one that really run the game looping
 def main():
+    score = 0
     clock = pygame.time.Clock() # create a clock type object that will control the game fps
-    bird = Bird(200, 200) # make the bird object
+    bird = Bird(230, 350) # make the bird object
+    base = Base(730)
+    pipes = [Pipe(600)]
     win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) # set the main window
     run = True # set the main loop condition variable
 
     while run: # initializes the loop
-        clock.tick(60) # set the frame to 30fps
+        clock.tick(30) # set the frame to 30fps
         for event in pygame.event.get(): # for each event(like mouse click, etc...) on game
             if event.type == pygame.QUIT: # if the event is that user click the big red X on top right corner
                 run = False # set the loop condition variable to false and ends the loop
 
-        bird.move() # move the bird
-        draw_window(win, bird) # draw the bird movement on the screen
+        #bird.move() # move the bird
+        add_pipe = False
+        rem = []
+        for pipe in pipes: # cause we want at least more than one pipe on the screen
+            if pipe.collide(bird):
+                print("COLLIDE!!!!!!")
+                pass
+            
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+            
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+                
+            pipe.move()
+        
+        if add_pipe:
+            print("Score:", score)
+            score += 1
+            pipes.append(Pipe(600))
+            
+        for r in rem:
+            pipes.remove(r)
+            
+        if bird.y + bird.img.get_height() >= 730:
+            pass
+            
+        
+        base.move() # move the base
+        draw_window(win, bird, pipes, base) # draw the bird movement on the screen
 
     pygame.quit() # quit the game
     quit() # qui the window
